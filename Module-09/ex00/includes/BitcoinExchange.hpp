269f0c2a85a8aec7600adeef44f742f4b3570ec5
Steven Lippert <slippert@3-C-3.42heilbronn.de>
2024-01-25 13:52:44 +0100

    ex00 cpp 09

--- Module-09/ex00/includes/BitcoinExchange.hpp
+++ Module-09/ex00/includes/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: slippert <slippert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 21:02:05 by slippert          #+#    #+#             */
-/*   Updated: 2024/01/23 19:15:49 by slippert         ###   ########.fr       */
+/*   Updated: 2024/01/25 13:23:57 by slippert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,208 +25,36 @@
 #define green "\033[1;32m"
 #define reset "\033[0m"
 
-struct CSVRow
-{
-	std::map<int, std::string> btc;
-	std::map<int, std::string> date;
-	std::string rate;
-	std::string error;
-};
-
 class BitcoinExchange
 {
 private:
-	std::map<std::string, CSVRow> data;
+	struct CSVRow
+	{
+		int year;
+		int month;
+		int day;
+		std::string date;
+		float rate;
+		float btc;
+		std::string error;
+	};
+	typedef std::map<int, CSVRow>::iterator iter;
+	std::map<int, CSVRow> map_input;
+	std::map<int, CSVRow> map_rates;
 	std::string input;
 	BitcoinExchange();
+	BitcoinExchange(const BitcoinExchange &ref);
+	BitcoinExchange &operator=(const BitcoinExchange &ref);
 
 public:
 	BitcoinExchange(std::string &_input);
-	BitcoinExchange(const BitcoinExchange &ref);
-	BitcoinExchange &operator=(const BitcoinExchange &ref);
 	~BitcoinExchange();
 	void StartExchange();
 	void LoadInputFile();
 	void LoadExchangeRates();
 	bool CheckDelims(std::string &column);
 	bool CheckDateFormat(std::string &column);
+	void CombineRateToInput();
+	void FindClosestDay(iter map, iter rates);
 	std::string trimWhitespaces(std::string &str);
 };
-
-BitcoinExchange::BitcoinExchange(std::string &_input) : input(_input)
-{
-	StartExchange();
-}
-
-// BitcoinExchange::BitcoinExchange(const BitcoinExchange &ref) {}
-
-BitcoinExchange &BitcoinExchange::operator=(const BitcoinExchange &ref)
-{
-	if (this != &ref)
-		*this = ref;
-	return (*this);
-}
-
-BitcoinExchange::~BitcoinExchange() {}
-
-void BitcoinExchange::LoadInputFile()
-{
-	std::ifstream file;
-	if ((file.open(input.c_str()), file.fail()))
-	{
-		std::cerr << "Error: could not open file." << std::endl;
-		return;
-	}
-
-	std::string line;
-	std::getline(file, line);
-	while (std::getline(file, line))
-	{
-		CSVRow row;
-
-		std::istringstream lineStream(line);
-		row.date.insert(std::make_pair(row.date.size(), lineStream.gcount()));
-		if (std::getline(lineStream, row.date, '|') && std::getline(lineStream, row.btc, '|'))
-		{
-			row.date = trimWhitespaces(row.date);
-			row.btc = trimWhitespaces(row.btc);
-
-			if (CheckDateFormat(row.date))
-			{
-				std::cerr << "Error: wrong formated date." << std::endl;
-				return;
-			}
-			long number = static_cast<long>(std::atol(row.btc.c_str()));
-			if (number < 0 || number > INT_MAX)
-			{
-				if (number > INT_MAX)
-					row.error = "Error: too large a number.";
-				else
-					row.error = "Error: not a positive number.";
-				row.date = "";
-			}
-			if (data.find(row.date) != data.end())
-				std::cout << "TEST" << row.date << std::endl;
-			std::pair<std::string, CSVRow> newRow(row.date, row);
-			data.insert(newRow);
-			// data.push_back(row);
-		}
-		else
-		{
-			row.error = "Error: bad input";
-			std::pair<std::string, CSVRow> newRow(row.date, row);
-			data.insert(newRow);
-		}
-	}
-	for (std::map<std::string, CSVRow>::iterator it = data.begin(); it != data.end(); ++it)
-	{
-		std::cout << it->first << " => " << it->second.btc << std::endl;
-	}
-}
-
-void BitcoinExchange::LoadExchangeRates()
-{
-	std::ifstream file;
-	if ((file.open("data.csv"), file.fail()))
-	{
-		std::cerr << "Error: could not open file." << std::endl;
-		return;
-	}
-	CSVRow row
-	std::string line;
-	if (std::getline(file, line))
-	{
-		std::istringstream lineStream(line);
-		if (std::getline(file, line, ','))
-			row.date = line;
-		std::cout << line << std::endl;
-		if (std::getline(file, line))
-			row.rate = line;
-		std::cout << line << std::endl;
-	}
-}
-void BitcoinExchange::StartExchange()
-{
-	LoadInputFile();
-	LoadExchangeRates();
-	// for (size_t i = 0; i < data.size(); i++)
-	// {
-	// 	std::cout << data[i].date << (data[i].btc != "" ? " => " : "") << data[i].btc << std::endl;
-	// }
-}
-
-bool BitcoinExchange::CheckDelims(std::string &column)
-{
-	size_t count = 0;
-	size_t pos = 0;
-
-	while (column[pos])
-	{
-		if (column[pos] == '-')
-			count++;
-		pos++;
-	}
-	if (count != 2)
-		return (false);
-	return (true);
-}
-
-bool BitcoinExchange::CheckDateFormat(std::string &column)
-{
-	int i = 0;
-	int count = 0;
-	int cycle = 0;
-	int delim = 0;
-	int size = column.size();
-	if (size != 10 || !CheckDelims(column))
-		return (true);
-	for (size_t i = 0; i < size; i++)
-	{
-		if (cycle == 0) // Check Year
-		{
-			if (column[i] == '-' && count == 4)
-			{
-				count = 0;
-				cycle++;
-				continue;
-			}
-			else if (std::isdigit(column[i]))
-				count++;
-			else
-				return (true);
-		}
-		if (cycle == 1) // Check Month
-		{
-			if (column[i] == '-' && count == 2)
-			{
-				count = 0;
-				cycle++;
-				continue;
-			}
-			else if (std::isdigit(column[i]))
-				count++;
-			else
-				return (true);
-		}
-		if (cycle == 2) // Check Days
-		{
-			if (std::isdigit(column[i]))
-				count++;
-			else
-				return (true);
-			if (count == 2)
-				break;
-		}
-	}
-	return (false);
-}
-
-std::string BitcoinExchange::trimWhitespaces(std::string &str)
-{
-	size_t start = str.find_first_not_of(" \t\n\r");
-	size_t end = str.find_last_not_of(" \t\n\r");
-
-	if (start == std::string::npos || end == std::string::npos)
-		return ("");
-	return (str.substr(start, end - start + 1));
-}
--- Module-09/ex00/srcs/BitcoinExchange.cpp
+++ Module-09/ex00/srcs/BitcoinExchange.cpp
@@ -6,8 +6,274 @@
 /*   By: slippert <slippert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/21 15:23:57 by slippert          #+#    #+#             */
-/*   Updated: 2024/01/21 17:24:33 by slippert         ###   ########.fr       */
+/*   Updated: 2024/01/25 13:51:49 by slippert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/BitcoinExchange.hpp"
+
+BitcoinExchange::BitcoinExchange(std::string &_input) : input(_input)
+{
+	StartExchange();
+}
+
+// BitcoinExchange::BitcoinExchange(const BitcoinExchange &ref) {}
+
+// BitcoinExchange &BitcoinExchange::operator=(const BitcoinExchange &ref)
+// {
+// 	if (this != &ref)
+// 		*this = ref;
+// 	return (*this);
+// }
+
+BitcoinExchange::~BitcoinExchange() {}
+
+void BitcoinExchange::LoadInputFile()
+{
+	std::ifstream file;
+	if ((file.open(input.c_str()), file.fail()))
+	{
+		std::cerr << "Error: could not open file." << std::endl;
+		return;
+	}
+
+	std::string line;
+	std::getline(file, line);
+	while (std::getline(file, line))
+	{
+		CSVRow row;
+
+		std::istringstream lineStream(line);
+		row.error = "";
+		std::string btc;
+		if (std::getline(lineStream, row.date, '|') && std::getline(lineStream, btc, '|'))
+		{
+			row.date = trimWhitespaces(row.date);
+			btc = trimWhitespaces(btc);
+
+			if (CheckDateFormat(row.date))
+			{
+				row.btc = 0.0;
+				row.error = "Error: bad input";
+				map_input.insert(std::make_pair(map_input.size(), row));
+			}
+			else
+			{
+				long long_Number = static_cast<long>(std::atol(btc.c_str()));
+				if (long_Number < 0 || long_Number > INT_MAX)
+				{
+					if (long_Number > INT_MAX)
+						row.error = "Error: too large a number.";
+					else
+						row.error = "Error: not a positive number.";
+					row.date = "";
+				}
+				float float_Number = static_cast<float>(std::atof(btc.c_str()));
+				row.btc = float_Number;
+				if (row.date != "")
+				{
+					std::string splitted;
+					std::istringstream dateStream(row.date);
+					std::getline(dateStream, splitted, '-');
+					row.year = static_cast<int>(std::atoi(splitted.c_str()));
+					std::getline(dateStream, splitted, '-');
+					row.month = static_cast<int>(std::atoi(splitted.c_str()));
+					std::getline(dateStream, splitted, '-');
+					row.day = static_cast<int>(std::atoi(splitted.c_str()));
+				}
+				map_input.insert(std::make_pair(map_input.size(), row));
+			}
+		}
+		else
+		{
+			row.btc = 0.0;
+			row.error = "Error: bad input";
+			map_input.insert(std::make_pair(map_input.size(), row));
+		}
+	}
+}
+
+void BitcoinExchange::LoadExchangeRates()
+{
+	std::ifstream file;
+	if ((file.open("data.csv"), file.fail()))
+	{
+		std::cerr << "Error: could not open file." << std::endl;
+		return;
+	}
+	std::string line;
+	std::getline(file, line);
+	while (std::getline(file, line))
+	{
+		CSVRow row;
+
+		std::istringstream lineStream(line);
+		std::string rate;
+		if (std::getline(lineStream, row.date, ',') && std::getline(lineStream, rate, ','))
+		{
+			row.date = trimWhitespaces(row.date);
+			rate = trimWhitespaces(rate);
+
+			if (row.date != "")
+			{
+				std::string splitted;
+				std::istringstream dateStream(row.date);
+				std::getline(dateStream, splitted, '-');
+				row.year = static_cast<int>(std::atoi(splitted.c_str()));
+				std::getline(dateStream, splitted, '-');
+				row.month = static_cast<int>(std::atoi(splitted.c_str()));
+				std::getline(dateStream, splitted, '-');
+				row.day = static_cast<int>(std::atoi(splitted.c_str()));
+			}
+			float float_Number = static_cast<float>(std::atof(rate.c_str()));
+			row.rate = float_Number;
+			map_rates.insert(std::make_pair(map_rates.size(), row));
+		}
+	}
+}
+
+void BitcoinExchange::FindClosestDay(iter it_map, iter rates)
+{
+	iter it_rates = rates;
+	iter closest_it = rates;
+	int daysDiffer = std::numeric_limits<int>::max();
+	std::cout << it_map->second.day << std::endl;
+	for (; it_rates != map_rates.end(); it_rates++)
+	{
+		if (std::abs(closest_it->second.day - it_map->second.day) < daysDiffer)
+		{
+			daysDiffer = std::abs(closest_it->second.day - it_map->second.day);
+			closest_it = it_rates;
+		}
+	}
+}
+
+void BitcoinExchange::CombineRateToInput()
+{
+	iter it_map;
+	iter it_rates;
+
+	for (it_map = map_input.begin(); it_map != map_input.end(); it_map++)
+	{
+		iter it_closest;
+		int daysDiffer = std::numeric_limits<int>::max();
+		for (it_rates = map_rates.begin(); it_rates != map_rates.end(); it_rates++)
+		{
+			if (it_map->second.date == it_rates->second.date)
+			{
+				it_map->second.rate = it_rates->second.rate;
+				break;
+			}
+			else if (it_map->second.year == it_rates->second.year && it_map->second.month == it_rates->second.month)
+			{
+				if (std::abs(it_rates->second.day - it_map->second.day) < daysDiffer)
+				{
+					daysDiffer = std::abs(it_rates->second.day - it_map->second.day);
+					it_closest = it_rates;
+					it_rates--;
+					// Check DayBefore if rate was lower
+					if (it_rates->second.rate < it_closest->second.rate && it_map->second.day < it_closest->second.day)
+						it_closest = it_rates;
+					it_rates++;
+					it_rates++;
+					// Check DayNext if rate was lower
+					if (it_rates->second.rate < it_closest->second.rate && it_map->second.day > it_closest->second.day)
+						it_closest = it_rates;
+					it_rates--;
+				}
+			}
+		}
+		it_map->second.rate = it_closest->second.rate;
+	}
+}
+
+void BitcoinExchange::StartExchange()
+{
+	LoadInputFile();
+	LoadExchangeRates();
+	CombineRateToInput();
+
+	for (std::map<int, CSVRow>::iterator it = map_input.begin(); it != map_input.end(); ++it)
+	{
+		if (it->second.error != "" && it->second.date == "")
+			std::cout << it->second.error << std::endl;
+		else if (it->second.error != "" && it->second.date != "")
+			std::cout << it->second.error << " => " << it->second.date << std::endl;
+		else
+			std::cout << it->second.date << " => " << it->second.btc << " => " << (it->second.btc * it->second.rate) << std::endl;
+	}
+}
+
+bool BitcoinExchange::CheckDelims(std::string &column)
+{
+	size_t count = 0;
+	size_t pos = 0;
+
+	while (column[pos])
+	{
+		if (column[pos] == '-')
+			count++;
+		pos++;
+	}
+	if (count != 2)
+		return (false);
+	return (true);
+}
+
+bool BitcoinExchange::CheckDateFormat(std::string &column)
+{
+	int count = 0;
+	int cycle = 0;
+	size_t size = column.size();
+	if (size != 10 || !CheckDelims(column))
+		return (true);
+	for (size_t i = 0; i < size; i++)
+	{
+		if (cycle == 0) // Check Year
+		{
+			if (column[i] == '-' && count == 4)
+			{
+				count = 0;
+				cycle++;
+				continue;
+			}
+			else if (std::isdigit(column[i]))
+				count++;
+			else
+				return (true);
+		}
+		if (cycle == 1) // Check Month
+		{
+			if (column[i] == '-' && count == 2)
+			{
+				count = 0;
+				cycle++;
+				continue;
+			}
+			else if (std::isdigit(column[i]))
+				count++;
+			else
+				return (true);
+		}
+		if (cycle == 2) // Check Days
+		{
+			if (std::isdigit(column[i]))
+				count++;
+			else
+				return (true);
+			if (count == 2)
+				break;
+		}
+	}
+	return (false);
+}
+
+std::string BitcoinExchange::trimWhitespaces(std::string &str)
+{
+	size_t start = str.find_first_not_of(" \t\n\r");
+	size_t end = str.find_last_not_of(" \t\n\r");
+
+	if (start == std::string::npos || end == std::string::npos)
+		return ("");
+	return (str.substr(start, end - start + 1));
+}
